5b0e15460d34948fa15f1d27552d75740c1f19b0
Rafael Monteiro Zancanaro <rafael.zancanaro@elotech.com.br>
2026-06-04 22:29:36 -0300

    [Feat]: Versão reduzida do jasper-reports

--- derivations/jaspersoft-studio.nix
+++ derivations/jaspersoft-studio.nix
@@ -1,51 +1,5 @@
-{
-  stdenv,
-  buildFHSEnv,
-  lib,
-  zlib,
-  libXext,
-  libXrender,
-  libXtst,
-  gtk3,
-  alsa-lib,
-  fontconfig,
-  dbus,
-  glib,
-  freetype,
-  writeShellScriptBin,
-}:
+{ stdenv, writeShellScriptBin }:
 
-let
-  version = "6.21.5";
-  pname = "jaspersoft-studio";
-  executable = "$HOME/.local/opt/js-studiocomm_${version}/Jaspersoft Studio";
-
-  # Criar o ambiente FHS
-  fhsEnv = buildFHSEnv {
-    name = "${pname}-${version}-env";
-
-    targetPkgs =
-      pkgs: with pkgs; [
-        zlib
-        libXext
-        libXrender
-        libXtst
-        gtk3
-        alsa-lib
-        fontconfig
-        dbus
-        glib
-        freetype
-        stdenv.cc.cc.lib
-      ];
-
-    # Usar aspas para lidar com espaço no caminho
-    runScript = writeShellScriptBin "run-jaspersoft" ''
-      exec "${executable}" "$@"
-    '';
-  };
-
-in
 writeShellScriptBin "jaspersoft-studio" ''
-  exec ${fhsEnv}/bin/run-jaspersoft "$@"
+  exec "$HOME/.local/opt/js-studiocomm_6.21.5/Jaspersoft Studio" "$@"
 ''
