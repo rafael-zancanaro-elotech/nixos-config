83b835f841fbb4dbf7f679b305de8b57eefe33a3
Rafael Monteiro Zancanaro <rafael.zancanaro@elotech.com.br>
2026-07-13 15:54:44 -0300

    [Feat] Adicionando web extensions no epiphany

--- home.nix
+++ home.nix
@@ -23,12 +23,6 @@
     xournalpp
   ];
 
-  dconf.settings = {
-    "org/gnome/epiphany/web" = {
-      enable-webextensions = true;
-    };
-  };
-
   programs.git = {
     enable = true;
     settings = {
